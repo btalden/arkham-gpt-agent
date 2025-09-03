f1f66204bcfaec95e99c7a597338f3bde5020856
Brandon Tadesse <brandontadesse@Brandons-MacBook-Pro.local>
2025-09-03 16:36:26 -0400

    force 8000 on startpy

--- start.py
+++ start.py
@@ -5,4 +5,4 @@ import uvicorn
 # Render injects PORT at runtime. If it's missing (local dev), default to 8000.
 port = int(os.environ.get("PORT", 8000))
 print(f"[start.py] Using PORT={port}")
-uvicorn.run("main:app", host="0.0.0.0", port=port)
+uvicorn.run("main:app", host="0.0.0.0", port=8000)
